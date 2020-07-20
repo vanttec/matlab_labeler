83cc2568025763be7efaf6fe945b383241d19aff
DRogerSC <musicfan211@hotmail.com>
2020-07-20 16:43:56 -0500

    Se mejoro el labeler

--- MultiClassLabeler.m
+++ MultiClassLabeler.m
@@ -1,27 +1,16 @@
 %% *LABEL CREATION*
 
-%This segment of the code focuses on 
+CurrFPath = matlab.desktop.editor.getActiveFilename;
+CurrFPath = CurrFPath(1:end-22);
+cd(CurrFPath)
+
+load('gangster_detector.mat');
 %%
-%This segment of code extracts the image size (x,y) for each image in the
-%training data
-imageSizes = table('Size', [height(training_data), 2], 'VariableTypes', {'double', 'double'}, 'VariableNames', {'X', 'Y'});
- for k=1:height(training_data)
-    img = imread(char(training_data.('imageFilename')(k))); 
-    ims = imshow(img);
-    % Get pixel dimensions of image 
-    imagePixelDimX = [ims.XData(2)]; % width in pixels
-    imagePixelDimY = [ims.YData(2)]; % height in pixels
-    imageSizes.('X')(k) = imagePixelDimX;
-    imageSizes.('Y')(k) = imagePixelDimY;
- end
- 
- %Here it adds the columns of size x and size y to each picture in the
- %training data
- 
- training_data = [training_data imageSizes];
-%%
+%This segment of the code focuses on creating the labels for the validation
+%images
+
 % Specify the folder where the files are.
-cd train_data/
+cd val_data/
 
 % Assign train data folder path to var myFolder
 myFolder = pwd;
@@ -30,101 +19,102 @@ myFolder = pwd;
 filePattern = fullfile(myFolder, '*.jpg');
 theFiles = dir(filePattern);
 %Create an array with the name of all files
-FilesTable = struct2table(theFiles) 
-ImageNames = FilesTable.name
+FilesTable = struct2table(theFiles)
+%% 
+% *This segment of code uses the detector for each image and stores the data 
+% in tables*
+
+%This segment of code extracts the image size (x,y) for each image in the
+%training data
+imageSizes = table('Size', [height(FilesTable), 2], 'VariableTypes', {'double', 'double'}, 'VariableNames', {'X', 'Y'});
+ for k=1:height(FilesTable)
+    img = imread(char(FilesTable.(1)(k))); 
+    ims = imshow(img);
+    
+    [bboxes,scores] = detect(detector,img, 'SelectStrongest',true);
+    
+    NoROI = isempty(bboxes);
+
+%Display the detection results and insert the bounding boxes for objects into the image.
+
+for i = 1:length(scores)
+   annotation = sprintf('Confidence = %.1f',scores(i));
+   img = insertObjectAnnotation(img,'rectangle',bboxes(i,:),annotation);
+end
+
+figure
+imshow(img)
+
+    % Get pixel dimensions of the image 
+    imagePixelDimX = [ims.XData(2)]; % width in pixels
+    imagePixelDimY = [ims.YData(2)]; % height in pixels
+    
+    if NoROI == false 
+        imageSizes.('BBX')(k) = bboxes(1,1);
+        imageSizes.('BBY')(k) = bboxes(1,2);
+        imageSizes.('BBW')(k) = bboxes(1,3);
+        imageSizes.('BBH')(k) = bboxes(1,4);
+    end
+    
+    imageSizes.('X')(k) = imagePixelDimX;
+    imageSizes.('Y')(k) = imagePixelDimY;
+ end
+ 
+ %Here it adds the columns of size x and size y to each picture in the
+ %training data
+ 
+ BboxesTable = [FilesTable.('name') imageSizes];
 
 %%
 %Makes a new table with ROI coordinates, width and height of the images in the
 %training session
-NewCoord = training_data(:, {'gangster', 'X', 'Y'})
+NewCoord = BboxesTable;
 
-%Here we converted the matlab format, which is x & y from the bottom left
+%Here we converted the matlab format, which is x & y from the top-left
 %corner to x and y from the center point. 
-for iX = 1:height(training_data)
-    NewCoord{iX, 1}{1}(1) = (NewCoord{iX, 'gangster'}{1}(1) + ((NewCoord{iX, 'gangster'}{1}(3))/2));
-    NewCoord{iX, 1}{1}(2) = (NewCoord{iX, 'gangster'}{1}(2) - ((NewCoord{iX, 'gangster'}{1}(4))/2));
+for iX = 1:height(NewCoord)
+    NewCoord{iX, 'BBX'} = NewCoord{iX, 'BBX'} + (NewCoord{iX, 'BBW'}/2);
+    NewCoord{iX, 'BBY'} = NewCoord{iX, 'BBY'} + (NewCoord{iX, 'BBH'}/2);
 end
 
 %Here we normalize the coordinates relative to the x size and y size
-for iX = 1:height(training_data)
-    NewCoord{iX, 1}{1}(1) = (NewCoord{iX, 'gangster'}{1}(1)/ NewCoord{iX, "X"});
-    NewCoord{iX, 1}{1}(2) = (NewCoord{iX, 'gangster'}{1}(2)/ NewCoord{iX, "Y"});
-    NewCoord{iX, 1}{1}(3) = (NewCoord{iX, 'gangster'}{1}(1)/ NewCoord{iX, "X"});
-    NewCoord{iX, 1}{1}(4) = (NewCoord{iX, 'gangster'}{1}(2)/ NewCoord{iX, "Y"});
+for iX = 1:height(NewCoord)
+    NewCoord{iX, 'BBX'} = NewCoord{iX, 'BBX'} / NewCoord{iX, 'X'};
+    NewCoord{iX, 'BBY'} = NewCoord{iX, 'BBY'} / NewCoord{iX, 'Y'};
+    NewCoord{iX, 'BBW'} = NewCoord{iX, 'BBW'} / NewCoord{iX, 'X'};
+    NewCoord{iX, 'BBH'} = NewCoord{iX, 'BBH'} / NewCoord{iX, 'Y'};
 end
 
 %%
-%Read the data and write it
-
-
 %For loop to write the .txt for each image 
-for iK = 1 : height(training_data)
+for iK = 1 : height(NewCoord)
     %Here we change the filename of each image to .txt
-    currTxtName = ImageNames{iK, 1}(1:end-4) + ".txt";
+    currTxtName = NewCoord.Var1{iK}(1:end-4) + ".txt";
     %Open a new file
-    TrainDataOut = fopen(currTxtName, 'a')
+a    TrainDataOut = fopen(currTxtName, 'w')
     %Print the converted coordinates to  the file
     fprintf(TrainDataOut, "0\t");
-    fprintf(TrainDataOut, '%.6f\t', NewCoord{iK,1}{1});
+    fprintf(TrainDataOut, '%.6f\t', NewCoord{iK,'BBX'});
+    fprintf(TrainDataOut, '%.6f\t', NewCoord{iK,'BBY'});
+    fprintf(TrainDataOut, '%.6f\t', NewCoord{iK,'BBW'});
+    fprintf(TrainDataOut, '%.6f\t', NewCoord{iK,'BBH'});
     fprintf(TrainDataOut, '\n');
     %Close the file
     fclose(TrainDataOut);
 end
-%%
-%Ejecuta Pol_detectors que crea las labels del policia y agrega sus
-%coordenadas a los .txt ya existentes
-
-cd ../
 %% Load Ground Truth - Police
 
 CurrFPath = matlab.desktop.editor.getActiveFilename;
-CurrFPath = CurrFPath(1:end-18);
+CurrFPath = CurrFPath(1:end-22);
 cd(CurrFPath)
 
-load('police_labels.mat')
+load('police_detector.mat');
 %%
-%Create training data from ground truth
-    %In case of multiple labels in the object:
-police_truth = selectLabels(gTruth,'police')
-%%
-%Create folder for training data
-%if isfolder(fullfile('training_data'))
-%   cd training_data
-%else
-%    mkdir training_data
-%end
-%addpath('training_data')
+%This segment of the code focuses on creating the labels for the validation
+%images
 
-%%
-%extracts a subset of ground truth dataset
-training_data = objectDetectorTrainingData(police_truth)
-summary(training_data)
-
-%train de ACF detector
-detector = trainACFObjectDetector(training_data,'NumStages',5)
-save('police_detector.mat','detector');
-%%
-%This segment of code extracts the image size (x,y) for each image in the
-%training data
-imageSizes = table('Size', [height(training_data), 2], 'VariableTypes', {'double', 'double'}, 'VariableNames', {'X', 'Y'});
- for k=1:height(training_data)
-    img = imread(char(training_data.('imageFilename')(k))); 
-    ims = imshow(img);
-    % Get pixel dimensions of image 
-    imagePixelDimX = [ims.XData(2)]; % width in pixels
-    imagePixelDimY = [ims.YData(2)]; % height in pixels
-    imageSizes.('X')(k) = imagePixelDimX;
-    imageSizes.('Y')(k) = imagePixelDimY;
- end
- 
- %Here it adds the columns of size x and size y to each picture in the
- %training data
- 
- training_data = [training_data imageSizes];
-
-%%
 % Specify the folder where the files are.
-cd train_data/
+cd val_data/
 
 % Assign train data folder path to var myFolder
 myFolder = pwd;
@@ -133,42 +123,86 @@ myFolder = pwd;
 filePattern = fullfile(myFolder, '*.jpg');
 theFiles = dir(filePattern);
 %Create an array with the name of all files
-FilesTable = struct2table(theFiles) 
-ImageNames = FilesTable.name
+FilesTable = struct2table(theFiles)
+%% 
+% *This segment of code uses the detector for each image and stores the data 
+% in tables*
+
+%This segment of code extracts the image size (x,y) for each image in the
+%training data
+imageSizes = table('Size', [height(FilesTable), 2], 'VariableTypes', {'double', 'double'}, 'VariableNames', {'X', 'Y'});
+ for k=1:height(FilesTable)
+    img = imread(char(FilesTable.(1)(k))); 
+    ims = imshow(img);
+    
+    [bboxes,scores] = detect(detector,img, 'SelectStrongest',true);
+    
+    NoROI = isempty(bboxes);
+
+%Display the detection results and insert the bounding boxes for objects into the image.
+
+for i = 1:length(scores)
+   annotation = sprintf('Confidence = %.1f',scores(i));
+   img = insertObjectAnnotation(img,'rectangle',bboxes(i,:),annotation);
+end
+
+figure
+imshow(img)
+
+    % Get pixel dimensions of the image 
+    imagePixelDimX = [ims.XData(2)]; % width in pixels
+    imagePixelDimY = [ims.YData(2)]; % height in pixels
+    
+    if NoROI == false 
+        imageSizes.('BBX')(k) = bboxes(1,1);
+        imageSizes.('BBY')(k) = bboxes(1,2);
+        imageSizes.('BBW')(k) = bboxes(1,3);
+        imageSizes.('BBH')(k) = bboxes(1,4);
+    end
+    
+    imageSizes.('X')(k) = imagePixelDimX;
+    imageSizes.('Y')(k) = imagePixelDimY;
+ end
+ 
+ %Here it adds the columns of size x and size y to each picture in the
+ %training data
+ 
+ BboxesTable = [FilesTable.('name') imageSizes];
 
 %%
 %Makes a new table with ROI coordinates, width and height of the images in the
 %training session
-NewCoord = training_data(:, {'police', 'X', 'Y'})
+NewCoord = BboxesTable;
 
-%Here we converted the matlab format, which is x & y from the bottom left
+%Here we converted the matlab format, which is x & y from the top-left
 %corner to x and y from the center point. 
-for iX = 1:height(training_data)
-    NewCoord{iX, 1}{1}(1) = (NewCoord{iX, 'police'}{1}(1) + ((NewCoord{iX, 'police'}{1}(3))/2));
-    NewCoord{iX, 1}{1}(2) = (NewCoord{iX, 'police'}{1}(2) - ((NewCoord{iX, 'police'}{1}(4))/2));
+for iX = 1:height(NewCoord)
+    NewCoord{iX, 'BBX'} = NewCoord{iX, 'BBX'} + (NewCoord{iX, 'BBW'}/2);
+    NewCoord{iX, 'BBY'} = NewCoord{iX, 'BBY'} + (NewCoord{iX, 'BBH'}/2);
 end
 
 %Here we normalize the coordinates relative to the x size and y size
-for iX = 1:height(training_data)
-    NewCoord{iX, 1}{1}(1) = (NewCoord{iX, 'police'}{1}(1)/ NewCoord{iX, "X"});
-    NewCoord{iX, 1}{1}(2) = (NewCoord{iX, 'police'}{1}(2)/ NewCoord{iX, "Y"});
-    NewCoord{iX, 1}{1}(3) = (NewCoord{iX, 'police'}{1}(1)/ NewCoord{iX, "X"});
-    NewCoord{iX, 1}{1}(4) = (NewCoord{iX, 'police'}{1}(2)/ NewCoord{iX, "Y"});
+for iX = 1:height(NewCoord)
+    NewCoord{iX, 'BBX'} = NewCoord{iX, 'BBX'} / NewCoord{iX, 'X'};
+    NewCoord{iX, 'BBY'} = NewCoord{iX, 'BBY'} / NewCoord{iX, 'Y'};
+    NewCoord{iX, 'BBW'} = NewCoord{iX, 'BBW'} / NewCoord{iX, 'X'};
+    NewCoord{iX, 'BBH'} = NewCoord{iX, 'BBH'} / NewCoord{iX, 'Y'};
 end
 
 %%
-%Read the data and write it
-
-
 %For loop to write the .txt for each image 
-for iK = 1 : height(training_data)
+for iK = 1 : height(NewCoord)
     %Here we change the filename of each image to .txt
-    currTxtName = ImageNames{iK, 1}(1:end-4) + ".txt";
+    currTxtName = NewCoord.Var1{iK}(1:end-4) + ".txt";
     %Open a new file
     TrainDataOut = fopen(currTxtName, 'a')
     %Print the converted coordinates to  the file
     fprintf(TrainDataOut, "1\t");
-    fprintf(TrainDataOut, '%.6f\t', NewCoord{iK,1}{1});
+    fprintf(TrainDataOut, '%.6f\t', NewCoord{iK,'BBX'});
+    fprintf(TrainDataOut, '%.6f\t', NewCoord{iK,'BBY'});
+    fprintf(TrainDataOut, '%.6f\t', NewCoord{iK,'BBW'});
+    fprintf(TrainDataOut, '%.6f\t', NewCoord{iK,'BBH'});
+    fprintf(TrainDataOut, '\n');
     %Close the file
     fclose(TrainDataOut);
 end
